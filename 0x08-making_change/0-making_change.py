aeea34713cbf1342e130c8453f9ccfda710ab29f
Mohamed Adel <72352419+Mohamed-Adel-A@users.noreply.github.com>
2024-03-04 00:02:38 +0200

    Create 0-making_change.py

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Given a pile of coins of different values,
+determine the fewest number of coins needed to meet a given amount total
+"""
+
+
+def makeChange(coins, total):
+    """
+    Return: fewest number of coins needed to meet total
+    If total is 0 or less, return 0
+    If total cannot be met by any number of coins you have, return -1
+    """
+
+    n = len(coins)
+    result = []
+    i = n - 1
+    while(i >= 0):
+        while (total >= coins[i]):
+            total -= coins[i]
+            result.append(coins[i])
+            i -= 1
+    return (i)
