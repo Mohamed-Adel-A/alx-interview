8ccedeab9067d2c8bc0fb11b896f5ece8a461bf1
Mohamed Adel <72352419+Mohamed-Adel-A@users.noreply.github.com>
2024-03-08 04:52:55 +0200

    Create 0-island_perimeter.py

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+a function def island_perimeter(grid): that returns the perimeter of the island described in grid
+"""
+
+
+def island_perimeter(grid):
+    """
+    grid is a list of list of integers:
+    0 represents water
+    1 represents land
+    Each cell is square, with a side length of 1
+    Cells are connected horizontally/vertically (not diagonally).
+    grid is rectangular, with its width and height not exceeding 100
+    """
+    
+    length = len(grid)
+    breath = len(grid[0])
+    perim = 0
+    for x in range(length):
+        for y in range(breath):
+            if grid[x][y] == 1:
+                perim += 4
+                if (y < breath - 1 and grid[x][y + 1] == 1):
+                    perim -= 2
+                if (x < length - 1 and grid[x + 1][y] == 1):
+                    perim -= 2
+    return perim
