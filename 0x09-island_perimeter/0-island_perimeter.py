f38842a9e03cd54e690b6ee8706b36169cfc5584
Mohamed Adel <72352419+Mohamed-Adel-A@users.noreply.github.com>
2024-03-08 05:22:59 +0200

    Update 0-island_perimeter.py

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-a function def island_perimeter(grid): that returns the perimeter of the island described in grid
+a function def island_perimeter(grid):
+that returns the perimeter of the island described in grid
 """
 
 
