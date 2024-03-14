ab1e12cc77b5169f7fa397e4314dec825d7b2787
Mohamed Adel <72352419+Mohamed-Adel-A@users.noreply.github.com>
2024-03-14 13:56:26 +0200

    Update 0-prime_game.py

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -20,10 +20,8 @@ def isWinner(x, nums):
         return None
     wins = {'Maria': 0, 'Ben': 0}
     for n in nums:
-        if not isinstance(n, int) or n <= 0:
-            return None
-        primes_count = sum(1 for i in range(1, n + 1) if is_prime(i))
-        if primes_count % 2 == 0:
+        prime_count = sum(1 for i in range(2, n + 1) if is_prime(i))
+        if prime_count % 2 == 0:
             wins['Ben'] += 1
         else:
             wins['Maria'] += 1
