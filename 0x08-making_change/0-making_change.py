8a791e0c01c31fc54699fc7279fc50d172d2d8ab
Mohamed Adel <72352419+Mohamed-Adel-A@users.noreply.github.com>
2024-03-14 14:10:38 +0200

    Testing new way

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -12,22 +12,24 @@ def makeChange(coins, total):
     If total cannot be met by any number of coins you have, return (-1)
     """
 
-    if (total <= 0):
-        return (0)
-    n = len(coins)
-    sub = list()
+    if total <= 0:
+        return 0
 
-    for i in range(total + 1):
-        sub.append(total + 1)
-    sub[0] = 0
+    # Create a list to store the minimum number of coins needed for each total from 0 to 'total'
+    dp = [float('inf')] * (total + 1)
 
-    for i in range(1, total + 1):
-        # For each coin we are given
-        for j in range(0, n):
-            if coins[j] <= i:
-                sub[i] = min(sub[i], sub[i - coins[j]] + 1)
+    # Base case: 0 coins needed for a total of 0
+    dp[0] = 0
 
-    if sub[total] > total:
-        return (-1)
+    # Iterate through each coin value
+    for coin in coins:
+        # For each coin value, iterate through each total from 'coin' to 'total'
+        for sub_total in range(coin, total + 1):
+            # Update the minimum number of coins needed for the current total
+            dp[sub_total] = min(dp[sub_total], dp[sub_total - coin] + 1)
 
-    return sub[total]
+    # If 'total' cannot be met by any combination of coins, return -1
+    if dp[total] == float('inf'):
+        return -1
+    else:
+        return dp[total]
