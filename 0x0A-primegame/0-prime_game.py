d56c36eae1ed9aa58588077d4ecd5c0fb3910857
Mohamed Adel <72352419+Mohamed-Adel-A@users.noreply.github.com>
2024-03-14 03:14:52 +0200

    Create 0-prime_game.py

---
+++ 0x0A-primegame/0-prime_game.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+def sieve_of_eratosthenes(n):
+    """Implementation of the Sieve of Eratosthenes algorithm."""
+    primes = [True] * (n+1)
+    primes[0] = primes[1] = False
+    p = 2
+    while p * p <= n:
+        if primes[p]:
+            for i in range(p * p, n+1, p):
+                primes[i] = False
+        p += 1
+    return [i for i in range(n+1) if primes[i]]
+
+def isWinner(x, nums):
+    """Determine the winner of the prime game."""
+    winners = {'Maria': 0, 'Ben': 0}
+    for n in nums:
+        primes = sieve_of_eratosthenes(n)
+        if len(primes) % 2 == 0:
+            winners['Ben'] += 1
+        else:
+            winners['Maria'] += 1
+
+    if winners['Maria'] == winners['Ben']:
+        return None
+    return max(winners, key=winners.get)
+
+if __name__ == "__main__":
+    print("Winner: {}".format(isWinner(5, [2, 5, 1, 4, 3])))
