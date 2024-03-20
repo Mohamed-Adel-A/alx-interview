58f0cf0a026044e4b9500dcb7cfd8e0e64babc5c
Mohamed Adel <72352419+Mohamed-Adel-A@users.noreply.github.com>
2024-03-20 17:09:46 +0200

    Update 0-pascal_triangle.py

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -7,20 +7,21 @@ Pascal's Triangle
 def pascal_triangle(n):
     """
     Generate Pascal's triangle up to the nth row.
-    
+
     Parameters:
         n (int): Number of rows to generate.
-        
+ 
     Returns:
-        list of lists: Pascal's triangle represented as a list of lists of integers.
+        list of lists:
+        Pascal's triangle represented as a list of lists of integers.
     """
     # Edge case: return an empty list if n is less than or equal to 0
     if n <= 0:
         return []
-    
+
     # Initialize Pascal's triangle with the first row
     triangle = [[1]]
-    
+
     # Generate subsequent rows
     for i in range(1, n):
         # Initialize the new row with the first element always being 1
@@ -32,5 +33,5 @@ def pascal_triangle(n):
         row.append(1)
         # Append the new row to the triangle
         triangle.append(row)
-    
+
     return triangle
